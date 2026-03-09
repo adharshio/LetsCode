e2c37a5e1706a20f9971fe3287144265cb78b7df
adharsh <adharshkandath@gmail.com>
2026-03-09 16:34:36 +0530

    completed day 15

---
+++ Day15/perfect.c
@@ -0,0 +1,10 @@
+bool checkPerfectNumber(int num) {
+    int count=0;
+    for(int i=1;i<num/2+1;i++){
+        if(num%i==0){
+            count+=i;
+        }
+    }
+    if(count==num)return true;
+    else return false;
+}
