97e039e6ed9170943fe296a7b7f3e513d66d1a2b
adharsh <adharshkandath@gmail.com>
2026-03-23 17:59:07 +0530

    completed day 29

---
+++ Day29/climbstairs.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n<=3:
+            return n
+        a=3
+        b=2
+        c=0
+        for i in range(3,n):
+            c=a+b
+            b=a
+            a=c
+        return c
+        
+        
